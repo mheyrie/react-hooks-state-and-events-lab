db79ab5f64cba4e71910ca190be168a4d713100e
Mary <omota4fidelity@gmail.com>
2023-09-05 21:24:16 +0100

    completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+//Use state declaration
+  const [inCart, setInCart] = useState(false)
+  const [addCart, setAddCart] = useState(true)
+
+  function handleCartStock() {
+    setInCart(!inCart)
+    setAddCart(!addCart)
+  }
+
+  const cart = inCart ? "Remove from Cart" : "Add to Cart"
   return (
-    <li className="">
+    <div>
+    <li className={ inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={ addCart ? "add" : "remove"} onClick={handleCartStock}>{cart}</button>
     </li>
+    </div>
   );
 }
 
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+const [selectCategory, setSelectCategory] = useState("all")
+
+function handleCategory(e){
+  setSelectCategory(e.target.value)
+}
+
+const filterItems = selectCategory === "all"
+ ? items 
+ : items.filter(item => item.category === selectCategory)
+
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleCategory} value={selectCategory} id="">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +25,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filterItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
